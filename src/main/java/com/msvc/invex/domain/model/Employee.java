d9ca4334b75f136639d1a01e00089fbd64192122
Diego Valencia <diego.val.maya@gmail.com>
2026-04-30 00:31:25 -0600

    add unit test

--- src/main/java/com/msvc/invex/domain/model/Employee.java
+++ src/main/java/com/msvc/invex/domain/model/Employee.java
@@ -1,13 +1,17 @@
 package com.msvc.invex.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
 	private Long id;
