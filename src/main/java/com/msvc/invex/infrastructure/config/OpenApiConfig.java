cbc3e6b1c3197796538725610dd09af66ce2b387
Diego Valencia <diego.val.maya@gmail.com>
2026-05-04 19:44:47 -0600

    Added Authorization for swagger

--- src/main/java/com/msvc/invex/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/msvc/invex/infrastructure/config/OpenApiConfig.java
@@ -1,19 +1,36 @@
 package com.msvc.invex.infrastructure.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    OpenAPI employeeOpenAPI() {
+    public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("Employee API")
-                        .version("1.0.0")
-                        .description("API para gestión de empleados"));
+                		.title("Employee API")
+                		.version("1.0.0")
+                		.description("API para gestión de empleados")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("Bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
